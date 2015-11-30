2067cf06d732ca38acd57b3acb1d0990d525f12f
Pooja Singh <poojakumari1389@gmail.com>
2015-11-30 17:01:37 +0530

    application changes

--- job/choices.py
+++ job/choices.py
@@ -24,6 +24,6 @@ LOCATION_CHOICES = ((0,"Not Mentioned"),)
 
 LOCATION_K2V = {tup[0]:tup[1] for tup in LOCATION_CHOICES}
 
-CANDIDATE_CITY_CHOICES = ((0,"Not Mentioned"),)
+CANDIDATE_CITY_CHOICES = ((-1,'Select City'),(0,"Not Mentioned"),(1,"Delhi"),(2,"Mumbai"))
 
-CANDIDATE_STATE_CHOICES = ((0,"Not Mentioned"),)
+CANDIDATE_STATE_CHOICES = ((-1,'Select State'),(0,"Not Mentioned"))
