020b640a36a33af1ee139577b74c8492ec06721b
animax453 <animesh.sharma@hindustantimes.com>
2015-11-30 14:40:57 +0530

    Template tags for job added

---
+++ job/templatetags/job_tags.py
@@ -0,0 +1,27 @@
+#python imports
+
+#django imports
+from django import template
+
+#local imports
+from choices import *
+
+#inter app imports
+
+#third party imports
+
+register = template.Library()
+
+@register.filter
+def key_value(key,key_type):
+    type_mapping = {'loc':LOCATION_K2V,
+                    'exp':EXP_K2V,
+                    'fa':FA_K2V,
+                    'sal':SALARY_K2V,
+                    }
+
+    k2v_dict = type_mapping.get('type')
+    if not k2v_dict:
+        return None
+
+    return k2v_dict.get(key)
